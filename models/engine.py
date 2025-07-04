335ed2473e0fe58b833ba2e677ac50d9a33465cb
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2025-07-04 12:09:27 -0300

    chore: removed python dotenv (using env in docker)

--- models/engine.py
+++ models/engine.py
@@ -1,14 +1,12 @@
 import os
 import typing
 
-from dotenv import load_dotenv
 from sqlmodel import Session, create_engine
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("DB_URL")
 
 ENGINE = create_engine(DATABASE_URL, echo=False)
+
 
 def get_db() -> typing.Generator[Session, None, None]:
     with Session(ENGINE) as session:
